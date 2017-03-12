ee083c9f3733abf0e40e2cc9d09e8566954aa0e5
Roberto Dinca <roberto.dinca@key4talent.com>
2017-03-12 21:24:30 +0100

    Create expandable details list directive

---
+++ src/tripDetails/expandable-details-list/expandableDetailsListDirective.js
@@ -0,0 +1,22 @@
+/**
+ * Created by r.dinca on 12/03/17.
+ */
+
+
+(function () {
+    'use strict';
+
+    angular.module("TripViewer")
+        .directive("expandableDetailsList", expandableDetailsList);
+
+    function expandableDetailsList() {
+        return {
+            restrict: 'E',
+            scope: {
+                'details': '=',
+                'units': '@'
+            },
+            templateUrl: '/src/tripDetails/expandable-details-list/expandable-details-list.html'
+        }
+    }
+})();
